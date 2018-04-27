690e8cd554f2492aedbc8d8a15597435f726eaca
zacanger <zac@zacanger.com>
2018-04-26 19:30:35 -0600

    lol have a umd build

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-module.exports = (style) => (element) => {
-  Object.keys(style).forEach((rule) => {
-    element.style[rule] = style[rule]
+// yeah i just hand-rolled my own UMD
+// with esm support, what of it
+
+;(function (global, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['exports'], factory)
+  } else if (typeof exports !== 'undefined') {
+    factory(exports)
+  } else {
+    var mod = {
+      exports: {}
+    }
+    factory(mod.exports)
+    global.actual = mod.exports
+  }
+})(this, function (exports) {
+  'use strict'
+  Object.defineProperty(exports, '__esModule', {
+    value: true
   })
-}
+  var style = function style (rules) {
+    return function (element) {
+      Object.keys(rules).forEach(function (rule) {
+        element.style[rule] = rules[rule]
+      })
+    }
+  }
+  exports.default = style
+  module.exports = exports['default']
+})
