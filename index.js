849f28530e5d0c106be1f729a17dc7ac27fe3c77
zacanger <zac@zacanger.com>
2018-04-26 19:34:57 -0600

    uh, undo that

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-// yeah i just hand-rolled my own UMD
-// with esm support, what of it
+var style = function style (rules) {
+  return function (element) {
+    Object.keys(rules).forEach(function (rule) {
+      element.style[rule] = rules[rule]
+    })
+  }
+}
 
-;(function (global, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['exports'], factory)
-  } else if (typeof exports !== 'undefined') {
-    factory(exports)
-  } else {
-    var mod = {
-      exports: {}
-    }
-    factory(mod.exports)
-    global.cssInJs = mod.exports
-  }
-})(this, function (exports) {
-  'use strict'
-  Object.defineProperty(exports, '__esModule', {
-    value: true
-  })
-  var cssInJs = function cssInJs (rules) {
-    return function (element) {
-      Object.keys(rules).forEach(function (rule) {
-        element.style[rule] = rules[rule]
-      })
-    }
-  }
-  exports.default = cssInJs
-  module.exports = exports['default']
-})
+module.exports = style
