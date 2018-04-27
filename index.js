19e760db6b6c2b55448e6b10dbbd079d199c15d9
zacanger <zac@zacanger.com>
2018-04-26 19:32:10 -0600

    rename in bundle

--- index.js
+++ index.js
@@ -11,20 +11,20 @@
       exports: {}
     }
     factory(mod.exports)
-    global.actual = mod.exports
+    global.cssInJs = mod.exports
   }
 })(this, function (exports) {
   'use strict'
   Object.defineProperty(exports, '__esModule', {
     value: true
   })
-  var style = function style (rules) {
+  var cssInJs = function cssInJs (rules) {
     return function (element) {
       Object.keys(rules).forEach(function (rule) {
         element.style[rule] = rules[rule]
       })
     }
   }
-  exports.default = style
+  exports.default = cssInJs
   module.exports = exports['default']
 })
