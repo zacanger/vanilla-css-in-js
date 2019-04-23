d296d5d0580c226d55c2d7d7cc8c9ce35ca98a67
zacanger <zac@zacanger.com>
2019-04-22 18:54:56 -0600

    Update

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-const style = (rules) => (el) => {
-  Object.keys(rules).forEach((rule) => {
-    el.style[rule] = rules[rule]
+function create (el) {
+  return document.createElement(el)
+}
+
+function style (rules) {
+  return function (el) {
+    Object.keys(rules)
+      .forEach(function (rule) {
+        el.style[rule] = rules[rule]
+      })
+  }
+}
+
+function createStyled (el) {
+  return function (rules) {
+    var e = create(el)
+    style(rules)(e)
+    return e
+  }
+}
+
+var _exports = style
+_exports.create = create
+_exports.style = style
+_exports.createStyled = createStyled
+
+// UMD-ish + ES modules
+if (typeof module !== 'undefined' && typeof exports === 'object') {
+  Object.defineProperty(exports, '__esmodule', {
+    value: true
   })
+  module.exports = _exports
+  module.exports.default = style
+} else if (typeof window !== 'undefined') {
+  window.__styled = _exports
+} else if (typeof global !== 'undefined' && typeof global === 'object') {
+  global.styled = _exports
 }
-
-const create = (el) => document.createElement(el)
-
-const createStyled = (el) => (rules) => {
-  const e = create(el)
-  style(rules)(e)
-  return e
-}
-
-module.exports = style
-module.exports.create = create
-module.exports.style = style
-module.exports.createStyled = createStyled
